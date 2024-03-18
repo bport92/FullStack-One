34f016ae1a127dc6679c6c54b1943a72cb4dc263
Brandon Porter <brandontporter@MacBook-Pro-2.local>
2024-03-18 18:48:33 -0400

    Static HTML to temp with JSON (((COMPLETE)))

--- travlr/app_server/routes/travel.js
+++ travlr/app_server/routes/travel.js
@@ -1,8 +1,12 @@
-var express = require('express');
-var router = express.Router();
-const controller = require('../controllers/travel');
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('.data/trips.json','utf8'));
 
-/* GET homepage. */
-router.get('/', controller.travel);
+/* GET travel view. */
+const travel = (req, res) => {
+    res.render ('travel', { title: 'Travlr Getaways' , trips});
 
-module.exports = router;
+};
+
+module.exports = {
+    travel
+};
