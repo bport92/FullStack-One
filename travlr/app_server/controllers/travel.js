e5a4cf63ecf39f6587d8fbfc872b316c79d913a3
Brandon Porter <brandontporter@MacBook-Pro-2.local>
2024-03-20 14:56:04 -0400

    Professor help ONE, JSON Functioning?

--- travlr/app_server/controllers/travel.js
+++ travlr/app_server/controllers/travel.js
@@ -1,11 +1,9 @@
-var fs = require('fs');
-var trips =
-JSON.parse(fs.readFileSync('data/trips.json','utf8'));
+const fs = require('fs');
+const trips = JSON.parse(fs.readFileSync('data/trips.json', 'utf8'));
 
 /* GET travel view */
-
 const travel = (req, res) => {
-    res.render('travel', {title: 'Travlr Getaways'})
+    res.render('travel', { title: 'Travlr Getaways', trips: trips });
 };
 
 module.exports = {
